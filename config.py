d3b623fc82536d84b4398caa4550bfacbdc86fbc
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-27 16:40:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"
 
 # UPI payment details
-UPI_ID = "as7726851@ybl"  # Replace with your personal UPI ID
+UPI_ID = "as772685@ybl"  # Replace with your personal UPI ID
